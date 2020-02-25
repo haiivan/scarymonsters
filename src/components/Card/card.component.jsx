c9f6ca30e0dee87020ebb18a7af3e13dfd9c2292
HaIvanVillegas <alberto_ha@live.com>
2020-02-25 00:33:59 -0600

    robots :s

--- src/components/Card/card.component.jsx
+++ src/components/Card/card.component.jsx
@@ -5,7 +5,7 @@ const Card = props => {
   return (
     <div className="card-container">
       <img
-        src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`}
+        src={`https://robohash.org/${props.monster.id}.png?set=set2?size=200x200`}
         alt="monster"
       />
       <h2>{props.monster.name}</h2>
--- src/components/CardList/card-list.component.jsx
+++ src/components/CardList/card-list.component.jsx
@@ -3,7 +3,6 @@ import "./card-list.style.css";
 import Card from "../Card/card.component";
 
 const CardList = props => {
-  console.log(props);
   return (
     <div className="card-list">
       {props.monsters.map(monster => (
