3cb45c27772191833bfa08e14fc2f80852b02c63
HaIvanVillegas <alberto_ha@live.com>
2020-03-02 11:27:22 -0600

    Updates

---
+++ precache-manifest.447f7ec8fc3ad1e0b58f2dce0def244e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d40c00554d00f099ad799baf01b12c0",
+    "url": "/scarymonsters/index.html"
+  },
+  {
+    "revision": "3740536863a6c1e7311a",
+    "url": "/scarymonsters/static/css/main.13a37fcb.chunk.css"
+  },
+  {
+    "revision": "d5b7dc0e95c86de42d59",
+    "url": "/scarymonsters/static/js/2.0b9572cb.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/scarymonsters/static/js/2.0b9572cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3740536863a6c1e7311a",
+    "url": "/scarymonsters/static/js/main.dc0dd2a1.chunk.js"
+  },
+  {
+    "revision": "90f7cd0602603dda35e9",
+    "url": "/scarymonsters/static/js/runtime-main.21ce2b43.js"
+  }
+]);
